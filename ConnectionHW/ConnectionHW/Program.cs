846158d3a980ecd9d2cbe174e607f4f878aba03d
olimeme <39192645+olimeme@users.noreply.github.com>
2018-11-13 19:39:24 +0600

    Add files via upload

---
+++ ConnectionHW/ConnectionHW/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ConnectedLevelPartOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["UserConnectionString"].ConnectionString;
+
+                connection.ConnectionString = connectionString;
+
+                connection.Open();
+
+                SqlCommand create = new SqlCommand();
+
+                create.Connection = connection;
+
+                create.CommandText = "create table Group(id int,name nvarchar(50));";
+
+                create.ExecuteNonQuery();
+            }
+
+        }
+    }
+}
